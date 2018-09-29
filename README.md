897b5dcba5f21a6fa066ca2a7c8de621c502a6e6
Joe Borg <cyborg101010@gmail.com>
2018-09-30 00:21:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dictionary
+
+## Prerequisite
+
+You'll need an account at [https://developer.oxforddictionaries.com](https://developer.oxforddictionaries.com).  Then use the ID and key as stated below.
+
+## Installation
+
+`cargo build`
+
+## Running
+
+`export DICTIONARY_APP_ID={Your ID}`
+
+`export DICTIONARY_APP_KEY={Your Key}`
+
+`./dictionary {word}`
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+extern crate json;
+extern crate reqwest;
+
+use std::env;
+
+fn underline(word: &String) -> String {
+    let mut underline = vec![];
+    for _ in 2..word.len() {
+        &underline.push("-");
+    }
+
+    return underline.join("");
+}
+
+fn display(parsed: &json::JsonValue) {
+    for result in parsed["results"].members() {
+        println!("{}", &result["id"]);
+        println!("{}", underline(&result["id"].dump()));
+        for lexical_entry in result["lexicalEntries"].members() {
+            for entry in lexical_entry["entries"].members() {
+                for sense in entry["senses"].members() {
+                    for definition in sense["definitions"].members() {
+                        println!("{}", definition);
+                    }
+                }
+            }
+        }
+        println!();
+    }
+}
+
+fn lookup(word: &String) -> json::JsonValue {
+    let client = reqwest::Client::new();
+    let url = &format!("https://od-api.oxforddictionaries.com:443/api/v1/entries/{}/{}", "en", word);
+
+    let app_id = match env::var("DICTIONARY_APP_ID") {
+        Ok(var) => var,
+        Err(_) => panic!("You must set DICTIONARY_APP_ID"),
+    };
+
+    let app_key = match env::var("DICTIONARY_APP_KEY") {
+        Ok(var) => var,
+        Err(_) => panic!("You must set DICTIONARY_APP_KEY")
+    };
+
+    let response = client.get(url)
+        .header("app_id", app_id)
+        .header("app_key", app_key)
+        .send().expect("error")
+        .text().expect("error");
+
+    return json::parse(&response).unwrap();
+}
+
+fn main() {
+    let word = match env::args().nth(1) {
+        Some(word) => word,
+        None => {
+            println!("Usage: dictionary <word>");
+            return;
+        }
+    };
+    let parsed = lookup(&word);
+    display(&parsed);
+}
